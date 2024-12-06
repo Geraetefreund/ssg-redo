b7585f0a3df2f8b038d057c03c5d9053167f9806
Geraetefreund <friteuse@arcor.de>
2024-12-06 08:28:15 +0000

    added tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -14,9 +14,22 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
 
     def test_eq_false(self):
-        node = TextNode("This is a text node", TextType.NORMAL, "https://boot.dev/")
-        node2 = TextNode("This is a text node", TextType.NORMAL)
+        node = TextNode("This is a text node", TextType.TEXT, "https://boot.dev/")
+        node2 = TextNode("This is a text node", TextType.TEXT)
         self.assertNotEqual(node, node2)
+
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev/")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev/)", repr(node)
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
