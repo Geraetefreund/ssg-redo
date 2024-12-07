fda047447d036f4aa54e01148363143375ebd714
Geraetefreund <friteuse@arcor.de>
2024-12-07 22:03:53 +0000

    add class ParentNode with tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,21 @@ class LeafNode(HTMLNode):
         if self.tag is None:
             return self.value
         return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def __repr__(self):
+        return f'ParentNode({self.tag}, {self.children}, {self.props})'
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None or self.children == []:
+            raise ValueError("ParentNode needs children")
+        result_string = ""
+        for child in self.children:
+            result_string += child.to_html()
+
+        return f'<{self.tag}{self.props_to_html()}>{result_string}</{self.tag}>'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -34,9 +34,53 @@ class TestLeafNode(unittest.TestCase):
         node = LeafNode('div', None)
         self.assertRaises(ValueError, node.to_html)
 
-    def _test_leafnode_no_tag(self):
+    def test_leafnode_no_tag(self):
         node = LeafNode(None, "Some value", {"class": "beer_class"})
         self.assertEqual(node.to_html(), f'{node.value}')
+
+
+class TestParentNode(unittest.TestCase):
+    def test_repr(self):
+        node = ParentNode("p", [LeafNode("b", "Bold Text inside a paragraph")])
+        self.assertEqual("ParentNode(p, [LeafNode(b, Bold Text inside a paragraph, None)], None)", node.__repr__())
+
+    def test_no_tag(self):
+        node = ParentNode(None,[LeafNode("b", "Bold Text inside a paragraph")])
+        #self.assertRaises(ValueError, node.to_html)
+        with self.assertRaises(ValueError) as error:
+            node.to_html()
+        self.assertEqual(str(error.exception), "Invalid HTML: no tag")
+
+    def test_no_children(self):
+        node = ParentNode("a", [])
+        with self.assertRaises(ValueError) as error:
+            node.to_html()
+        self.assertEqual(str(error.exception), "ParentNode needs children")
+
+
+    def test_ParentNode_to_html(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(node.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span><b>grandchild</b></span></div>")
+
+    def test_to_html_props(self):
+        node = ParentNode("a", [LeafNode("b", "Bold text within a link")],
+                          props={'href': 'https://boot.dev'})
+        self.assertEqual(node.to_html(),
+                         '<a href="https://boot.dev"><b>Bold text within a link</b></a>')
 
 if __name__ == "__main__":
     unittest.main()
