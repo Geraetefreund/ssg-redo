62d8c0caa3308d44850140fad79491d0d2b8219b
Geraetefreund <friteuse@arcor.de>
2024-12-07 13:29:42 +0000

    add class LeafNode and tests.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -19,7 +19,24 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(node.tag, "div")
         self.assertEqual(node.value, "I wish I could read")
         self.assertEqual(node.children, None)
-        self.assertEqual((node.props), None)
+        self.assertEqual(node.props, None)
+
+class TestLeafNode(unittest.TestCase):
+    def test_p_to_html(self):
+        node = LeafNode('p', 'This is a paragraph of text.')
+        self.assertEqual(node.to_html(), '<p>This is a paragraph of text.</p>')
+        
+    def test_a_to_html(self):
+        node = LeafNode('a', 'Click me!', {"href": "https://www.google.com/"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com/">Click me!</a>')
+
+    def test_leafnode_no_value(self):
+        node = LeafNode('div', None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def _test_leafnode_no_tag(self):
+        node = LeafNode(None, "Some value", {"class": "beer_class"})
+        self.assertEqual(node.to_html(), f'{node.value}')
 
 if __name__ == "__main__":
     unittest.main()
